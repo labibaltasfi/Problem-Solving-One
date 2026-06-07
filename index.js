78ff13be23d424b697c36970c7747853a2156de3
labibaltasfi <labibaltasfi5@gmail.com>
2026-06-07 22:26:10 +0600

    problem 17

--- index.js
+++ index.js
@@ -268,3 +268,18 @@ for (let i = 0; i < text.length; i++) {
     }
   }
 }
+
+//---------------------------------------------------------------------- problem 17
+// FizzBuzz
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
