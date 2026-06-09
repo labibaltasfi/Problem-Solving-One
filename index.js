0bd9ab4a9d0a62d1bf20318a120a80586fdb7186
labibaltasfi <labibaltasfi5@gmail.com>
2026-06-09 23:23:23 +0600

    problem 18

--- index.js
+++ index.js
@@ -283,3 +283,16 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
   }
 }
+
+//---------------------------------------------------------------------- problem 18
+// Sum of even numbers
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+
+console.log(sum);
