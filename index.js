bcefb52a8c9c4462246acbf5522a6f16c82a357a
labibaltasfi <labibaltasfi5@gmail.com>
2026-07-12 22:33:46 +0600

    problem 40

--- index.js
+++ index.js
@@ -585,3 +585,19 @@ for (let i = 0; i < numbers.length - 1; i++) {
 }
 
 console.log(sorted);
+
+
+//---------------------------------------------------------------------- problem 40
+// Count frequency of a number
+
+let numbers = [1, 2, 2, 3, 2, 4];
+let target = 2;
+let count = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] === target) {
+    count++;
+  }
+}
+
+console.log(count);
